95b5c69e04623b3b35619fa22bf8423bf67f594b
AndySu <a07458666@gmail.com>
2022-05-04 20:52:33 +0800

    [green]add buy ,price

--- apps/bowling/src/app/potter.ts
+++ apps/bowling/src/app/potter.ts
@@ -1,3 +1,12 @@
 export class Potter {
     private bookCoubt: number[] = [];
+
+    buy(bookID: number, bookCount: number) {
+        this.bookCoubt[bookID] += bookCount;
+    }
+
+    get price() {
+        let price = 0;
+        return price;
+      }
 }
