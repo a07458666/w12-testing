74f7f2a38ef6ff1138e043f77a9e9ce248cc935c
AndySu <a07458666@gmail.com>
2022-05-04 21:06:19 +0800

    [green]price

--- apps/bowling/src/app/potter.ts
+++ apps/bowling/src/app/potter.ts
@@ -1,12 +1,17 @@
 export class Potter {
-    private bookCoubt: number[] = [];
-
-    buy(bookID: number, bookCount: number) {
-        this.bookCoubt[bookID] += bookCount;
+    private bookBuyCount: number[] = [0, 0, 0, 0, 0, 0];
+    private bookSize = 5;
+    private bookPrice = 8;
+    buy(id: number, count: number) {
+        this.bookBuyCount[id] += count;
     }
 
     get price() {
         let price = 0;
+        for (let idx = 0; idx < this.bookSize; idx++)
+        {
+            price += (this.bookPrice * this.bookBuyCount[idx])
+        }
         return price;
-      }
+    }
 }
