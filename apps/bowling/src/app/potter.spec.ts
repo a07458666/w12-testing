231343245e40e7d483565ab9c1a680b67a17e25d
AndySu <a07458666@gmail.com>
2022-05-04 20:51:39 +0800

    [red]buy 0 book

--- apps/bowling/src/app/potter.spec.ts
+++ apps/bowling/src/app/potter.spec.ts
@@ -1,7 +1,18 @@
 import { Potter } from './potter';
 
 describe('Potter', () => {
+
+  let potter: Potter;
+
+  beforeEach(() => {
+    potter = new Potter();
+  });
+
   it('should create an instance', () => {
     expect(new Potter()).toBeTruthy();
+  });
+
+  test('buy zero book', () => {
+    expect(potter.price).toBe(0);
   });
 });
--- apps/bowling/src/app/potter.ts
+++ apps/bowling/src/app/potter.ts
@@ -1,2 +1,3 @@
 export class Potter {
+    private bookCoubt: number[] = [];
 }
