234024eae0c655a4bf4cf0d2602ec4804517475a
AndySu <a07458666@gmail.com>
2022-05-07 12:51:34 +0800

    [green]remove console

--- apps/bowling/src/app/potter.ts
+++ apps/bowling/src/app/potter.ts
@@ -11,8 +11,6 @@ export class Potter {
     combine(books: number[]){
         books.sort().reverse();
         const sum = books.reduce((partialSum, a) => partialSum + a, 0);
-        console.log("books o", books);
-        console.log("sum ", sum); // 6
         if (sum == 0)
         {
             return 0;
@@ -25,16 +23,9 @@ export class Potter {
             if (books[i] > 0)
             {
                 books[i]--;
-                console.log("iiiii --",sum, i);
                 set_price = ((i + 1) * this.bookPrice) * this.discount[i];
-                console.log("books be", books);
                 other_price = this.combine(Array.from(books));
-                console.log("best_price old", best_price);
                 best_price = Math.min(other_price + set_price, best_price);
-                console.log("set_price ", set_price);
-                console.log("other_price ", other_price);
-                console.log("best_price ", best_price);
-                console.log("books af", books);
             }
             else{
                 break;
@@ -46,12 +37,7 @@ export class Potter {
     get price() {
         let price = 0;
         this.bookBuyCount.sort().reverse();
-        console.log(this.bookBuyCount);
         price = this.combine(Array.from(this.bookBuyCount));
-        // for (let idx = 0; idx < this.bookSize; idx++)
-        // {
-        //     price += (this.bookPrice * this.bookBuyCount[idx])
-        // }
         return price;
     }
 }
